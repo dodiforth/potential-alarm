c69ecf990e1246764754777aa1a5783cc6d1ec9d
Dowon Kim <dowoniscool@yahoo.com>
2024-11-09 14:35:30 +0100

    first commit

---
+++ alarmPeace01/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  alarmPeace01
+//
+//  Created by Dowon Kim on 03/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ alarmPeace01/alarmPeace01App.swift
@@ -0,0 +1,17 @@
+//
+//  alarmPeace01App.swift
+//  alarmPeace01
+//
+//  Created by Dowon Kim on 03/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct alarmPeace01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
